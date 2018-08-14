97c201f4a4ef4ffb02f8759d37806fd3ddb5c01e
kworker <kworker@riseup.net>
2018-08-14 01:57:58 +0000

    Log to json instead of an ill-defined format
    json lets us more easily read this info by machine later.
    Since we're using json, the 'delimiter' argument is pointless
    and removed.
    
    Note that by spec SSIDs are 0-32 octets, no specified encoding.
    We base64 it for storage because json doesn't support raw
    bytes, and we need something we can encode.

--- probemon.py
+++ probemon.py
@@ -6,6 +6,8 @@ import argparse
 import netaddr
 import sys
 import logging
+import json
+import base64
 from scapy.all import *
 from pprint import pprint
 from logging.handlers import RotatingFileHandler
@@ -16,7 +18,7 @@ DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
 
 DEBUG = False
 
-def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid, rssi):
+def build_packet_callback(time_fmt, logger, mac_info, ssid, rssi):
 	def packet_callback(packet):
 		
 		if not packet.haslayer(Dot11):
@@ -27,47 +29,51 @@ def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid, rssi):
 		if packet.type != 0 or packet.subtype != 0x04:
 			return
 
-		# list of output fields
-		fields = []
+		# data we've observed
+		result = {}
 
 		# determine preferred time format 
 		log_time = str(int(time.time()))
 		if time_fmt == 'iso':
-			log_time = datetime.datetime.now().isoformat()
+			log_time = datetime.now().isoformat()
 
-		fields.append(log_time)
+		result["log_time"] = log_time
 
 		# append the mac address itself
-		fields.append(packet.addr2)
+		result["mac"] = packet.addr2
 
 		# parse mac address and look up the organization from the vendor octets
 		if mac_info:
 			try:
 				parsed_mac = netaddr.EUI(packet.addr2)
-				fields.append(parsed_mac.oui.registration().org)
+				result["mac_info"] = parsed_mac.oui.registration().org
 			except netaddr.core.NotRegisteredError, e:
-				fields.append('UNKNOWN')
+				result["mac_info"] = 'UNKNOWN'
 
 		# include the SSID in the probe frame
+		# NOTE: by spec the SSID is 0-32 octets, no encoding enforced. 0-length SSID is the wildcard.
+		# to encode these bytes in JSON, we need to use base64, which guarantees we can encode as utf-8
 		if ssid:
-			fields.append(packet.info)
+			result["ssid"] = base64.b64encode(packet.info)
 			
 		if rssi:
 			rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
-			fields.append(str(rssi_val))
+			result["rssi"] = rssi_val
 
-		logger.info(delimiter.join(fields))
+		res_string = ''
+		res_string = json.dumps(result)
+
+		logger.info(res_string)
 
 	return packet_callback
 
 def main():
 	parser = argparse.ArgumentParser(description=DESCRIPTION)
-	parser.add_argument('-i', '--interface', help="capture interface")
+	parser.add_argument('-i', '--interface', help="capture interface. Should be monitoring, eg: 'mon0'")
 	parser.add_argument('-t', '--time', default='iso', help="output time format (unix, iso)")
 	parser.add_argument('-o', '--output', default='probemon.log', help="logging output location")
 	parser.add_argument('-b', '--max-bytes', default=5000000, help="maximum log size in bytes before rotating")
 	parser.add_argument('-c', '--max-backups', default=99999, help="maximum number of log files to keep")
-	parser.add_argument('-d', '--delimiter', default='\t', help="output field delimiter")
 	parser.add_argument('-f', '--mac-info', action='store_true', help="include MAC address manufacturer")
 	parser.add_argument('-s', '--ssid', action='store_true', help="include probe SSID in output")
 	parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
@@ -89,7 +95,7 @@ def main():
 	if args.log:
 		logger.addHandler(logging.StreamHandler(sys.stdout))
 	built_packet_cb = build_packet_callback(args.time, logger, 
-		args.delimiter, args.mac_info, args.ssid, args.rssi)
+		args.mac_info, args.ssid, args.rssi)
 	sniff(iface=args.interface, prn=built_packet_cb, store=0)
 
 if __name__ == '__main__':
